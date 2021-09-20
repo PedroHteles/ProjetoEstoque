640faf8e32dcbd01935fed54115781fa668bee96
PedroHteles <pedroteles849@gmail.com>
2021-09-20 17:46:43 -0300

    att

--- camera.py
+++ camera.py
@@ -1,6 +1,6 @@
 from sys import intern
 import cv2
-# from pyzbar.pyzbar import decode
+from pyzbar.pyzbar import decode
 import numpy as np
 import json
 
@@ -13,59 +13,69 @@ produto ='{"p":1234,"e":1}'
 endereco = '{"end":1}'
 
 
-try:
+# try:
 
-    endereco = json.loads(endereco)
-    valida = endereco["end"]
-    try:
-        produto = json.loads(produto)
-        valida = produto["p"],produto["e"]
+#     endereco = json.loads(endereco)
+#     valida = endereco["end"]
+#     try:
+#         produto = json.loads(produto)
+#         valida = produto["p"],produto["e"]
         
-        print(f'''Endereco: {endereco["end"]}''')
-        print(f'''Produto:'{produto["p"]}, Endereco-Do-Produto:{produto["e"]}''')
-        print(f'''Posicao-Pedido: {produto["p"]} Endereco: {produto["e"]}''', produto["e"] == endereco["end"])
+#         print(f'''Endereco: {endereco["end"]}''')
+#         print(f'''Produto:'{produto["p"]}, Endereco-Do-Produto:{produto["e"]}''')
+#         print(f'''Posicao-Pedido: {produto["p"]} Endereco: {produto["e"]}''', produto["e"] == endereco["end"])
 
-    except:
-        print('qr code produto errado')
+#     except:
+#         print('qr code produto errado')
     
-except:
-    print('qr code endereco errado')
+# except:
+#     print('qr code endereco errado')
 
 
 
 # errados = []
 # corretos = []
-# produtos = []
-# enderecos = []
-# itens = []
-# print(len(itens))
+produtos = []
+enderecos = []
+itens = []
+teste = {}
 
 
+img = cv2.imread('final.png')
 
-# while True:
-#     validacao, frame = webcam.read()
+while True:
+    validacao, frame = webcam.read()
 
-#     for barcode in decode(frame):
-#         myData = barcode.data.decode('utf-8')
-#         if myData not in itens:
-#             qr = json.loads(myData)
-#             if qr["p"] and qr["e"] in qr:
-#                   produto = json.loads(myData)
-#             elif  qr["e"] in qr:
-#                   enderecos = json.loads(myData)
-#             else:
-#                   print('erro ao ler qr ')
-#             # if produto["p"] and produto["e"] in produto:
-#             #     print(produto["p"],produto["e"])
-#             # else if (produto["p"] and produto["e"] not in produto):
-#             #     print('erro ao ler o qr code')
-#             # if enderecos["end"]:
-#             #     print(enderecos["end"])
+    for barcode in decode(img):
+        myData = barcode.data
+        try:   
+            qr = json.loads(myData)
+            if qr not in itens:
+                if len(qr) == 2:
+                    if {'p':qr["p"],'e':qr["e"]} not in produtos:
+                        product = {}
+                        product['p'] = qr['p']
+                        product['e'] = qr['e']
+                        produtos.append(product)  
+                elif len(qr) == 1:
+                    if {'end':qr["end"]} not in enderecos:
+        
+                        enderecos.append({'end':qr["end"]})
+
+        except:
+            print('erro')
+    break
+
+# if produtos['e'] == enderecos['end']:
+#     print('ok')
+print(produtos)
+print(enderecos)
+
 
         
-#         pts = np.array([barcode.polygon], np.int32)
-#         pts = pts.reshape((-1,1,2))
-#         cv2.polylines(frame,[pts],True,(255,0,255),5)  
-#     cv2.imshow("teste",frame)
-#     cv2.waitKey(5)
+    #     pts = np.array([barcode.polygon], np.int32)
+    #     pts = pts.reshape((-1,1,2))
+    #     cv2.polylines(frame,[pts],True,(255,0,255),5)  
+    # cv2.imshow("teste",frame)
+    # cv2.waitKey(5)
 
