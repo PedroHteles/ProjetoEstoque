aa88dce9d95c443c1cb2fe43ae05dbea49fc3e9e
unknown <pedroteles849@gmail.com>
2021-09-20 00:40:56 -0300

    att1 20/09/21

--- camera.py
+++ camera.py
@@ -9,17 +9,24 @@ webcam = cv2.VideoCapture(0)
 webcam.set(3,640)
 webcam.set(4,480)
 
-
-
+itens =[]
+print(len(itens))
 while True:
     validacao, frame = webcam.read()
-    itens =[]
+
     for barcode in decode(frame):
         myData = barcode.data.decode('utf-8')
-        itens.append({myData})
-        print(myData)
+        if myData not in itens: 
+            teste = myData.split(',')
+            if 'p' in myData:
+                print('p:',teste[0].split(':')[1])
+            if 'e' in myData:
+                print('e:',teste[1].split(':')[1])
+
+        
         pts = np.array([barcode.polygon], np.int32)
         pts = pts.reshape((-1,1,2))
-        cv2.polylines(frame,[pts],True,(255,0,255),5)   
+        cv2.polylines(frame,[pts],True,(255,0,255),5)  
     cv2.imshow("teste",frame)
     cv2.waitKey(5)
+
