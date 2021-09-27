c9841d6eeb65d5d7125507ef068b4c387cf79fa8
PedroHteles <pedroteles849@gmail.com>
2021-09-27 12:50:58 -0300

    att27/08/21 v3

--- camera.py
+++ camera.py
@@ -1,5 +1,6 @@
 from sys import intern
 import cv2
+from numpy.core.fromnumeric import amax
 from pyzbar.pyzbar import decode
 import numpy as np
 import json
@@ -10,27 +11,20 @@ import math
 
 
 
-img = cv2.imread('agrvai.png')
+img = cv2.imread('dinal.png')
 webcam = cv2.VideoCapture(0)
 webcam.set(3,640)
 webcam.set(4,480)
 
 
-
-
             
 def lerqr(x):
-
     produtos = []
     endereco = []
     for barcode in decode(x):
         (x, y, w, h) = barcode.rect
-    
         barcodeData = barcode.data.decode("utf-8")
         barcodeType = barcode.type
-        
-    
-
         try:
             qr = json.loads(barcodeData)
             if (qr['p'],qr['e']) not in produtos and qr['p'] > 0:
@@ -43,47 +37,20 @@ def lerqr(x):
                 endereco.append(p)
                 cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
                 text = "endereco ({})".format(barcodeType)
-            
-            
+            else:
+                return 'error'
             cv2.putText(img, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 0, 255), 2)
-
-
-
-
-            # if qr in produtos and qr['p'] > 0:
-            #     cv2.rectangle(img, (x, y), (x + w, y + h), (0,250,125), 2)
-            #     text = "te ({})".format(barcodeType)
-        
-            
-            # if np.array(result).size > 0:
-            #     print('teste')
         except:
             text = "erro qr '{}' nao e valido ({})".format(barcodeData,barcodeType)
             cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
             cv2.putText(img, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 0, 255), 2)
-
         result =  np.where(produtos[0][1] in endereco)
+    return np.array(result).size 
         
-    return np.array(result).size
-        
-
-
-
-
-
-
-
 while True:
     validacao, frame = webcam.read()
     x = img
-    print(lerqr(x))
-
-    
-
-
-
-
-
+    print(lerqr(x) > 0)
 
     
     cv2.imshow("produtos",img)
