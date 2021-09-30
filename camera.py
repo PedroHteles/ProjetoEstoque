9718184fd94220f8f577c30e546fdf280b81cc98
PedroHteles <pedroteles849@gmail.com>
2021-09-30 17:34:24 -0300

    v1

--- camera.py
+++ camera.py
@@ -1,18 +1,23 @@
 import cv2
 from pyzbar.pyzbar import decode
 import json
+import numpy as np
+
+
+erroEnderecoAux = []
+erroProdutoAux = []
+
 
 produtosAux = []
-endereco = []
-erroAux = []
+enderecoAux = []
+
+produtosValidos = []
+enderecosValidos = []
 
 def lerqr(x,height,width):
-
     start_point = (int(width / 6), int(height  / 6))
     end_point = (int(width / 1.20), int(height / 1.20))
-    color = (255, 0, 0)
-    thickness = 2
-    cv2.rectangle(img, start_point, end_point, color, thickness)
+    cv2.rectangle(img, start_point, end_point,(255,0,0),2)
     
 
     for barcode in decode(x):
@@ -20,68 +25,38 @@ def lerqr(x,height,width):
         barcodeData = barcode.data.decode("utf-8")
         barcodeType = barcode.type
         leituraArea = x < (end_point[0] - w) and x > start_point[0] and y < (end_point[1] - h ) and y > start_point[1] 
+        
         try:
             qr = json.loads(barcodeData)
             enderecoQr = qr['e']
-            qrCodigoProduto = qr['p']     
+            qrCodigoProduto = qr['p']
+            if leituraArea:    
 
-            if leituraArea and enderecoQr not in endereco and qrCodigoProduto == 0 and len(endereco) == 0:
-                e = ((enderecoQr),(x,y))
-                endereco.append(e)
-                print(' endereco encontrado')
-            elif endereco == []:
-                print('aguardando qr Endereco')    
+                # ///////////////////////////////////////// valida qr //////////////////////////////////////////// 
+                if qrCodigoProduto == 0 and enderecoQr not in enderecoAux:
+                    print(' endereco encontrado')
+                    enderecoAux.append(enderecoQr)
+                elif (qrCodigoProduto,enderecoQr) not in produtosAux and qrCodigoProduto > 0 and enderecoAux !=[] and enderecoQr in enderecoAux:
+                    print(' produto encontrado')
+                    if enderecoQr in enderecoAux  and (qrCodigoProduto,enderecoQr) not in produtosAux:
+                        produtosAux.append((qrCodigoProduto,enderecoQr))
 
+                # ////////////////////////////////////////////// corzinha no qr validado ////////////////////////////////
+                lst = np.array(produtosAux)
+                result = np.where(lst == enderecoQr)
 
-            if leituraArea and (qrCodigoProduto,enderecoQr) not in produtosAux and qrCodigoProduto > 0 and len(produtosAux) == 0 and endereco !=[]:
-                p = ((qrCodigoProduto,enderecoQr),(x,y))
-                produtosAux.append(p)
-                print(' produto encontrado')
-            elif produtosAux == []:
-                print('aguardando qr Produto')
-
-            #  //////////////////////////////////// VERIFICACAO QR ///////////////////////////////   
-
-            pCpQrAux = produtosAux[0][0][0]
-            pAux = produtosAux[0][0]
-            
-            eQrAux = endereco[0][0]
-
-            # verifica se existe mais de 1 qr na leitura
-            if leituraArea and qrCodigoProduto == 0 and enderecoQr != eQrAux and len(erroAux) == 0 :
-                print('mais de 1 endereco encontrado')
-                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 20)
-                e = ((enderecoQr),(x,y))
-                erroAux.append(e)
+                if enderecoQr in enderecoAux  and (qrCodigoProduto,enderecoQr)  in produtosAux:
+                    if (qrCodigoProduto,enderecoQr) not in produtosValidos:
+                        produtosValidos.append((qrCodigoProduto,enderecoQr))
+                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 15)
+                elif qrCodigoProduto == 0 and enderecoQr in enderecoAux and np.array(result).size > 0 :
+                    if enderecoQr not in enderecosValidos:
+                        enderecosValidos.append(enderecoQr)
+                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 15)
+        
             else:
-                erroAux = []
+                cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0,255), 20)
 
-            if leituraArea and qrCodigoProduto > 0 and qrCodigoProduto != pCpQrAux:
-                print('mais de 1 Produto encontrado')
-                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-                
-                
-            # valida qr endereco
-            if erroAux == []:
-                if leituraArea and qrCodigoProduto == 0 and enderecoQr == eQrAux:
-                    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
-
-                elif leituraArea and qrCodigoProduto == 0 and enderecoQr != eQrAux:
-                    print('erro qr endereco')
-                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0,255), 5)
-
-                # valida qr produto
-                if leituraArea and qrCodigoProduto > 0 and (qrCodigoProduto,enderecoQr) == pAux and enderecoQr == eQrAux:
-                    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 5)
-
-                elif leituraArea and qrCodigoProduto > 0 and (qrCodigoProduto,enderecoQr) == pAux and enderecoQr != eQrAux:
-                    print('Produto no endereco errado')
-                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0,255), 5)
-            else:
-                if(leituraArea):
-                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0,255), 5)
-                    if leituraArea and qrCodigoProduto == 0:
-                        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 15)
                     
         except:
             if(leituraArea):
@@ -92,12 +67,18 @@ while True:
     img = cv2.imread('./img/dirnal.png')
     height, width, channels = img.shape
     lerqr(img,height,width)
+    print(produtosValidos,enderecosValidos)
+    
   
     cv2.imshow("camera",img)
     key = cv2.waitKey(5)
     if key == 27:
         break
-    
+    if key == 122:
+        erroEnderecoAux = []
+        erroProdutoAux = []
+
+
     
 
-            
+        
